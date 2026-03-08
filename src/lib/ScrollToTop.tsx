02ab17988ee11bab1a0303e93241cd991a488ef2
RangSheet <ranggaa606@gmail.com>
2026-03-08 21:54:42 +0700

    remove comments

--- src/lib/ScrollToTop.tsx
+++ src/lib/ScrollToTop.tsx
@@ -3,20 +3,19 @@ import { useEffect } from "react";
 import { useLocation } from "react-router-dom";
 
 const ScrollToTop = () => {
-  const { pathname, hash } = useLocation(); // Extracts the current pathname
+  const { pathname, hash } = useLocation();
 
   useEffect(() => {
     if (!hash) {
-      // Scrolls the window to the top whenever the pathname changes
       window.scrollTo({
         top: 0,
         left: 0,
-        behavior: "smooth", // Optional: use 'instant' for no animation
+        behavior: "smooth",
       });
     }
-  }, [pathname]); // The effect runs again whenever 'pathname' changes
+  }, [pathname]);
 
-  return null; // This component doesn't render anything
+  return null;
 };
 
 export default ScrollToTop;
